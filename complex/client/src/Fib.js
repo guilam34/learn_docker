afe9c02f0f3f29f6c3ee809262cc07b7b176c588
guilam34 <guillaumelam34@gmail.com>
2019-04-29 14:44:38 -0700

    Fix server

--- complex/client/src/Fib.js
+++ complex/client/src/Fib.js
@@ -35,6 +35,9 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
+    if (this.state.seenIndexes !== Array) {
+      return this.state.seenIndexes;
+    }
     return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
